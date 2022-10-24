cd23faf1f4bed2e6c7e80bf0b67e4d14979b13e6
spicyhotnoodles <pietro.ciuci@gmail.com>
2022-10-24 16:21:42 +0200

    Initial commit

---
+++ Ideally/IdeallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  IdeallyApp.swift
+//  Ideally
+//
+//  Created by Pietro Ciuci on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct IdeallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Models/Gift.swift
@@ -0,0 +1,16 @@
+//
+//  Gift.swift
+//  Ideally
+//
+//  Created by Pietro Ciuci on 24/10/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct Gift: Idea, Identifiable {
+    var 
+    
+    
+}
+
